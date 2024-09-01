94d6d2a7e5c02b5d9f5b49e57808fbadb7a9050b
bansok3 <neokalpa3@gmail.com>
2024-09-02 07:00:14 +0900

    squre commit

---
+++ Python/project/Untitled-1.py
@@ -0,0 +1,13 @@
+# user = input("")
+# if user.islower():
+#     print(user.upper())
+# else:
+#     print(user.lower())
+
+리스트 = ["가", "나", "다", "라"]
+
+for aa in 리스트[::-1] :
+    print(aa)
+
+
+
---
+++ Python/project/squre.py
@@ -0,0 +1,43 @@
+class Square:
+    def __init__(self):
+        self.square = int(input('넓이를 구하고 싶은 사각형의 숫자를 써주세요.\n 1.직사각형 2.평행사변형 3.사다리꼴 \n >>>'))
+
+        if self.square == 1:
+            print('직사각형 함수는 rect()입니다.')
+
+        elif self.square == 2:
+            print('평행사변형 함수는 par()입니다.')
+        
+        elif self.square == 3:
+            print('사다리꼴 함수는 trape()입니다.')
+        
+        else:
+            print('1, 2, 3 중에서 다시 입력해주세요')
+
+    def rect(self):
+        width, vertical = map(int, input('가로, 세로를 입력하세요. 예시 : 가로,세로\n >>>').split(','))
+        area = width * vertical
+        result = '직사각형의 넓이는 : ' + str(area)
+        return result
+
+    def par(self):
+        base, height = map(int, input('밑변, 높이를 입력하세요. 예시 : 밑변,높이\n >>>').split(','))
+        area = base * height
+        result = '평행사변형의 넓이는 : ' + str(area)
+        return result
+
+    def trape(self):
+        base, top, height = map(int, input('밑변, 윗변, 높이를 입력하세요. 예시 : 밑변,윗변,높이\n >>>').split(','))
+        area = ((base + top) * height) / 2
+        result = '사다리꼴의 넓이는 : ' + str(area)
+        return result
+
+# 예시 사용법
+square = Square()
+
+if square.square == 1:
+    print(square.rect())
+elif square.square == 2:
+    print(square.par())
+elif square.square == 3:
+    print(square.trape())
